1d373ac9116c31d38291d79a248130008cf556d2
Cristian Mos <moscristian@gmail.com>
2012-10-07 16:35:30 +0200

    prima clasa care va controla lansarea cientilor

--- src/controlTrafic/ApplicationController.java
+++ src/controlTrafic/ApplicationController.java
@@ -9,5 +9,8 @@ package controlTrafic;
  * @author cmos
  */
 public class ApplicationController {
-    
+    public static void main(String[] args){
+        //aceasta este clasa de baza care va controla lansarea clientilor
+        
+    }
 }

fbf488998a3bffc1065210553c03223266fa6c4a
Cristian Mos <moscristian@gmail.com>
2012-10-07 16:44:29 +0200

    am adaugat clasa tren cu metoda getId

---
+++ src/controlTrafic/Tren.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlTrafic;
+
+/**
+ *
+ * aceasta clasa are o structura simpla
+ * deocamdata nu retinem numai Id-ul trenului
+ * 
+ */
+public class Tren {
+    
+    private int id;
+    
+    Tren(int id ){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+}
